8d2c2c67481102ff6565b08bac5d0357198d3742
David Romero <48596887+dl-romero@users.noreply.github.com>
2024-08-25 21:08:24 -0700

    update

--- neptune_exporter.py
+++ neptune_exporter.py
@@ -6,7 +6,6 @@ import time
 import math
 import socket
 import os
-import logging.config
 import requests
 from pathlib import Path
 import uvicorn
@@ -15,11 +14,22 @@ from fastapi.responses import PlainTextResponse, RedirectResponse
 import yaml
 from neptune_modules import neptune_apex
 from neptune_modules import neptune_fusion
+import logging.config
 
 def setup_logger(name, log_file, level=logging.INFO):
-    # Setup logging machanism
+    """
+    Set up the logger for the application.
+
+    Args:
+        name (str): The name of the logger.
+        log_file (str): The path to the log file.
+        level (int): The logging level.
+
+    Returns:
+        logger (logging.Logger): The configured logger.
+    """
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler = logging.FileHandler(log_file)        
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -37,19 +47,19 @@ except:
     exit()
 
 app = FastAPI(
-    title = "Apex Exporter",
-    summary = "Prometheus Exporter for the Neptune Apex.",
-    description = "https://github.com/dl-romero/apex_exporter",
-    version = "1.0",
-    contact = {
+    title="Apex Exporter",
+    summary="Prometheus Exporter for the Neptune Apex.",
+    description="https://github.com/dl-romero/apex_exporter",
+    version="1.0",
+    contact={
         "name": "dromero.dev",
         "url": "https://dromero.dev"
     },
-    license_info = {
-        "name": "MIT License",
+    license_info={
+        "name": "License",
         "url": "https://github.com/dl-romero/apex_exporter/blob/main/LICENSE",
     },
-    openapi_tags = [
+    openapi_tags=[
         {
             "name": "Apex",
             "description": "Get Apex Metrics in Prometheus Format",
@@ -63,30 +73,65 @@ app = FastAPI(
             "description": "View Apex Exporter Logs",
         }
     ]
-    )
+)
 
 @app.get("/metrics/apex", response_class=PlainTextResponse, tags=["Apex"])
 async def apex_prometheus_metrics(target, auth_module):
+    """
+    Get Apex metrics in Prometheus format.
+
+    Args:
+        target (str): The IP address of the Apex device.
+        auth_module (str): The authentication module.
+
+    Returns:
+        str: The Prometheus metrics.
+    """
     apex_direct = neptune_apex.APEX(apex_ip=target, auth_module=auth_module)
     return apex_direct.prometheus_metrics()
 
 @app.get("/metrics/fusion", response_class=PlainTextResponse, tags=["Fusion"])
 async def fusion_prometheus_metrics(data_max_age, fusion_apex_id):
+    """
+    Get Fusion metrics in Prometheus format.
+
+    Args:
+        data_max_age (int): The maximum age of the data.
+        fusion_apex_id (str): The ID of the Fusion Apex.
+
+    Returns:
+        str: The Prometheus metrics.
+    """
     apex_fusion = neptune_fusion.FUSION(fusion_apex_id, data_max_age)
     return apex_fusion.prometheus_metrics()
 
 @app.get("/logs/", response_class=PlainTextResponse, tags=["Logs"])
 async def apex_exporter_logs(log_file_name):
+    """
+    View Apex Exporter logs.
+
+    Args:
+        log_file_name (str): The name of the log file.
+
+    Returns:
+        str: The contents of the log file.
+    """
     if log_file_name in ['neptune.log', 'exporter.log', 'apex.log']:
         log_file = str(os.path.dirname(__file__)) + '/logs/' + 'neptune_exporter.log'
         with open(log_file, "r") as open_file:
             return "".join(open_file.readlines())
     else:
-        "Log name not found. Use one of ('neptune.log', 'exporter.log', 'apex.log') without any quotes."
+        return "Log name not found. Use one of ('neptune.log', 'exporter.log', 'apex.log') without any quotes."
 
-@app.get("/",include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def documentation_home_page():
-    return (RedirectResponse(url='/docs'))
+    """
+    Redirect to the documentation home page.
+
+    Returns:
+        RedirectResponse: The redirect response.
+    """
+    return RedirectResponse(url='/docs')
 
 if __name__ == "__main__":
     server_hostname = socket.gethostname()
